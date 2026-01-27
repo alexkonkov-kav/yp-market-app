ed874bcc59a16b1c0b55a04785d489fa684bcb88
Alex Al <Alex@al.com>
2026-01-27 22:25:05 +0300

    добавит обработку Get запроса OrderController

--- src/main/java/com/market/controller/OrderController.java
+++ src/main/java/com/market/controller/OrderController.java
@@ -5,7 +5,9 @@ import com.market.service.OrderService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -20,9 +22,19 @@ public class OrderController {
     }
 
     @GetMapping
-    public String getOrderPage(Model model) {
+    public String getOrdersPage(Model model) {
         List<OrderResponseDto> dtos = orderService.getAllOrders();
         model.addAttribute("orders", dtos);
         return "orders";
+    }
+
+    @GetMapping("/{id}")
+    public String getOrderPage(@PathVariable Long id,
+                               @RequestParam(value = "newOrder", defaultValue = "false") boolean newOrder,
+                               Model model) {
+        OrderResponseDto dto = orderService.getOrderResponseById(id);
+        model.addAttribute("order", dto);
+        model.addAttribute("newOrder", newOrder);
+        return "order";
     }
 }
--- src/main/java/com/market/service/OrderService.java
+++ src/main/java/com/market/service/OrderService.java
@@ -2,8 +2,11 @@ package com.market.service;
 
 import com.market.dto.OrderResponseDto;
 import com.market.mapper.OrderMapper;
+import com.market.model.Order;
 import com.market.repository.OrderRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,9 +22,22 @@ public class OrderService {
         this.orderMapper = orderMapper;
     }
 
+    @Transactional(readOnly = true)
+    public Order getOrderById(Long id) {
+        return orderRepository.findByIdlWithItems(id)
+                .orElseThrow(() -> new EntityNotFoundException("Not found Order with ID: " + id));
+    }
+
+    @Transactional(readOnly = true)
     public List<OrderResponseDto> getAllOrders() {
         return orderRepository.findAllWithItems().stream()
                 .map(orderMapper::mapToOrderResponseDto)
                 .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public OrderResponseDto getOrderResponseById(Long id) {
+        Order order = getOrderById(id);
+        return orderMapper.mapToOrderResponseDto(order);
     }
 }
