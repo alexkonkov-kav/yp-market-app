bd63a089052efc3a80dced88e9c676b0808efcca
Alex Al <Alex@al.com>
2026-02-18 23:06:50 +0300

    изменит тесты сервиса

--- src/test/java/com/market/service/ItemServiceTest.java
+++ src/test/java/com/market/service/ItemServiceTest.java
@@ -95,43 +95,4 @@ public class ItemServiceTest {
                 .verifyComplete();
         verify(itemRepository, times(1)).count();
     }
-
-//    @Test
-//    void get_Item_By_Id() {
-//        Long itemId = 1L;
-//        Item mockItem = new Item("яблоко", "яблоко красное", "images/apple.jpg", 50);
-//        mockItem.setId(itemId);
-//        when(itemRepository.findById(itemId)).thenReturn(Optional.of(mockItem));
-//        Item result = itemService.getItemById(itemId);
-//        assertThat(result).isNotNull();
-//        assertThat(result.getId()).isEqualTo(itemId);
-//        assertThat(result.getTitle()).isEqualTo("яблоко");
-//        verify(itemRepository, times(1)).findById(itemId);
-//    }
-//
-//    @Test
-//    void get_Item_By_Id_Not_Found() {
-//        Long itemId = 999L;
-//        when(itemRepository.findById(itemId)).thenReturn(Optional.empty());
-//        assertThatThrownBy(() -> itemService.getItemById(itemId))
-//                .isInstanceOf(EntityNotFoundException.class)
-//                .hasMessageContaining("Not found Item with ID: " + itemId);
-//        verify(itemRepository).findById(itemId);
-//    }
-//
-//    @Test
-//    void find_By_Title_Or_Description() {
-//        String searchText = "яблоко";
-//        Pageable pageable = PageRequest.of(0, 5, Sort.by("title"));
-//        Item mockItem = new Item("яблоко", "яблоко красное", "images/apple.jpg", 50);
-//        Page<Item> mockPage = new PageImpl<>(List.of(mockItem), pageable, 1);
-//        when(itemRepository.findByTitleContainingIgnoreCaseOrDescriptionContainingIgnoreCase(
-//                eq(searchText), eq(searchText), eq(pageable))).thenReturn(mockPage);
-//        Page<Item> result = itemService.findByTitleOrDescription(searchText, searchText, pageable);
-//        assertThat(result).isNotNull();
-//        assertThat(result.getContent()).hasSize(1);
-//        assertThat(result.getTotalElements()).isEqualTo(1);
-//        assertThat(result.getContent().getFirst().getTitle()).isEqualTo("яблоко");
-//        verify(itemRepository).findByTitleContainingIgnoreCaseOrDescriptionContainingIgnoreCase(searchText, searchText, pageable);
-//    }
 }
